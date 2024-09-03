2d22a998b9a1e5bd248ab1f58c0912cf68bbdcc8
Anhelina <fadeevalina20@gmail.com>
2024-09-03 14:41:49 +0300

    add

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
-  base: '/goit-react-hw-01/',
+plugins: [react()],
+build: {
+sourcemap: true,
+}
 });
